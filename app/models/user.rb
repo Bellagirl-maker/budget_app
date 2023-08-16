36a4c56a2c3731c0d344d49f58bc8f551155dc89
Isabella <isabellaotoo25@gmail.com>
2023-08-16 22:52:39 +0000

    Update style for budget app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :expenses, dependent: :destroy, foreign_key: :author_id
   has_many :categories, dependent: :destroy, foreign_key: :author_id
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
