31ea2e600ccba412c9be10825e29be108f6fc341
Isabella <isabellaotoo25@gmail.com>
2023-08-17 00:54:14 +0000

    update styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :expenses, dependent: :destroy, foreign_key: :author_id
   has_many :categories, dependent: :destroy, foreign_key: :author_id
-  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
